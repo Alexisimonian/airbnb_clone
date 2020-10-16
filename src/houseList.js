c6c6b35ef196e3fe09ca492dbd4bcc5624e6dc6b
Alexis Simonian <alexis792.as@gmail.com>
2020-10-16 11:23:25 +0200

    setting up crypt

--- src/houseList.js
+++ src/houseList.js
@@ -3,20 +3,11 @@ import Offer from "./houseOffer";
 export default class List {
   constructor() {
     this.houseList = [];
-    this.accumulator = 0;
   }
 
   create(title, price, description, availability, address, image) {
-    let offer = new Offer(
-      title,
-      price,
-      description,
-      availability,
-      address,
-      image
+    this.houseList.push(
+      new Offer(title, price, description, availability, address, image)
     );
-    offer.id = this.accumulator;
-    this.houseList.push(offer);
-    this.accumulator += 1;
   }
 }
