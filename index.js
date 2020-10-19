11becf15c111e7d0ba4b4e514c91c860522de1fe
Alexis Simonian <alexis792.as@gmail.com>
2020-10-19 21:29:04 +0200

    setting up database query

--- index.js
+++ index.js
@@ -1,9 +1,11 @@
+const dbId = require("./src/dbInfos.js");
+//import User from "./src/user.js";
 const express = require("express");
-const session = require("expres-session");
+const session = require("express-session");
 const cors = require("cors");
 const bodyParser = require("body-parser");
-const path = require("path");
 const mysql = require("mysql");
+const { request } = require("express");
 const app = express();
 
 let port = process.env.PORT;
@@ -11,7 +13,30 @@ if (port == null || port == "") {
   port = 3000;
 }
 
+let connection = mysql.createConnection({
+  host: "localhost",
+  user: dbId.username,
+  password: dbId.password,
+  database: "Airbnb_clone_test",
+});
+connection.connect(function (err) {
+  if (err) {
+    return console.log("error:" + err.message);
+  }
+  console.log("Connected to mysql server");
+});
+
 app.use(express.static("./")).use(cors());
+app.use(
+  session({
+    secret: "secret",
+    resave: true,
+    saveUninitialized: true,
+  })
+);
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+
 app.listen(port, () => {
   console.log(`listening on port: ${port}`);
 });
@@ -24,11 +49,33 @@ app.get("/login", (req, res) => {
   res.sendFile(__dirname + "/public/login.html");
 });
 
-app.post("/listing", (req, res) => {
+app.post("/login", (req, res) => {
+  let username = req.body.username;
   let email = req.body.email;
   let password = req.body.psw;
-  res.sendFile(__dirname + "/public/listing.html");
+  if (username && email && password) {
+    connection.query(
+      `SELECT * FROM users WHERE name = '${username}' AND password = '${password}'`,
+      function (err, result, fields) {
+        if (result.length > 0) {
+          req.session.loggedin = true;
+          res.redirect("/listing");
+        } else {
+          res.send(
+            "Incorrect email and/or password.<br> If you do not have an account please sign up."
+          );
+        }
+        res.end();
+      }
+    );
+  }
 });
+
+app.get("/signup", (req, res) => {
+  res.sendFile(__dirname + "/public/signup.html");
+});
+
+app.post("/signup", (req, res) => {});
 
 app.get("/listing", (req, res) => {
   res.sendFile(__dirname + "/public/listing.html");
