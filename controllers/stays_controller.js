a2b9e6fc122c801e7b8cab4c0feefbde0d47711e
Alexis Simonian <alexis792.as@gmail.com>
2020-11-02 15:39:14 +0100

    setting up image upload

--- controllers/stays_controller.js
+++ controllers/stays_controller.js
@@ -1,6 +1,8 @@
 const express = require("express");
 const bodyParser = require("body-parser");
 const ejs = require("ejs");
+const http = require("http");
+const fs = require("fs");
 const { Stays } = require("../src/stays");
 
 const stays = new Stays();
@@ -16,6 +18,16 @@ staysRoutes.get("/stays", function (req, res) {
 
 staysRoutes.get("/stays/new", function (req, res) {
   res.render("newStay", { errors: "" });
+});
+
+staysRoutes.post("/stays", function (req, res) {
+  let title = req.body.title;
+  let address = req.body.address;
+  let price = req.body.price;
+  let avaibilityFrom = req.body.startDate;
+  let avaibilityTo = req.body.endDate;
+  let description = req.body.description;
+  let image = req.body.imageFile;
 });
 
 module.exports = { StaysRoutes: staysRoutes };
