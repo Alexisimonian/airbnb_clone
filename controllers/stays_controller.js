a318d904c0318585ac6fbf98a4084e8179344167
Alexis Simonian <alexis792.as@gmail.com>
2020-11-02 17:35:06 +0100

    capable of uploading photo

--- controllers/stays_controller.js
+++ controllers/stays_controller.js
@@ -3,6 +3,11 @@ const bodyParser = require("body-parser");
 const ejs = require("ejs");
 const http = require("http");
 const fs = require("fs");
+const multer = require("multer");
+const path = require("path");
+
+const upload = multer({ dest: "../public/images/" });
+
 const { Stays } = require("../src/stays");
 
 const stays = new Stays();
@@ -20,14 +25,19 @@ staysRoutes.get("/stays/new", function (req, res) {
   res.render("newStay", { errors: "" });
 });
 
-staysRoutes.post("/stays", function (req, res) {
+staysRoutes.post("/stays", upload.single("imageFile"), function (req, res) {
   let title = req.body.title;
   let address = req.body.address;
   let price = req.body.price;
   let avaibilityFrom = req.body.startDate;
   let avaibilityTo = req.body.endDate;
   let description = req.body.description;
-  let image = req.body.imageFile;
+  let image = req.file.originalname;
+  let tempPath = req.file.path;
+  let targetPath = path.join(__dirname, `../public/images/${image}`);
+  fs.rename(tempPath, targetPath, (err) => {
+    if (err) throw err;
+  });
 });
 
 module.exports = { StaysRoutes: staysRoutes };
