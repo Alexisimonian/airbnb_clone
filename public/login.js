e83ab358f93690bddf0ad0a4be336be2976c3487
Alexis Simonian <alexis792.as@gmail.com>
2020-11-06 15:58:02 +0100

    refactoring

--- public/login.js
+++ public/login.js
@@ -1,6 +1,6 @@
 $(document).ready(function () {
-  $("#loginForm").on("submit", function (ev) {
-    ev.preventDefault();
+  $("#loginForm").on("submit", function (e) {
+    e.preventDefault();
     let data = $(this).serialize();
     $.ajax({
       type: "post",
@@ -8,7 +8,7 @@ $(document).ready(function () {
       data: data,
       dataType: "text",
       success: function () {
-        ev.target.submit();
+        e.target.submit();
       },
       error: function (data) {
         $("#errors").text(data.responseText);
