83e90e554647a2192905822bf19a428a9e16f0e6
Alexis Simonian <alexis792.as@gmail.com>
2020-11-09 20:11:46 +0100

    added images previews

--- public/newStay.js
+++ public/newStay.js
@@ -1,8 +1,26 @@
 $(document).ready(function () {
+  let imagesPreview = function (input, placeToPreview) {
+    if (input.files) {
+      let filesAmount = input.files.length;
+      for (i = 0; i > filesAmount; i++) {
+        let reader = new FileReader();
+        reader.onload = function (event) {
+          $($.parseHTML("<img>"))
+            .attr("src", event.target.result)
+            .appendTo(placeToPreview);
+        };
+        reader.readAsDataURL(input.files[i]);
+      }
+    }
+  };
+  $("#imageFiles").on("change", function () {
+    imagesPreview(this, ".preview-images");
+  });
   $("#newStayForm").on("submit", function (e) {
+    e.preventDefault();
     $.ajax({
       type: "post",
-      url: "/newstay",
+      url: "/upload",
       success: function () {
         window.location.href = "http://localhost:3000/stays";
       },
