a074fbf909d4660b92c4ae5472b7432c41810eac
Alexis Simonian <alexis792.as@gmail.com>
2020-11-16 18:15:03 +0100

    initialized dynamic account creation

--- controllers/account_controller.js
+++ controllers/account_controller.js
@@ -17,12 +17,49 @@ accountRoutes.get("/register", (req, res) => {
   res.sendFile("register.html", { root: "public" });
 });
 
+accountRoutes.post("/username-validation", async (req, res) => {
+  let username = req.body.username;
+  let existingUsernames = await user.existingUsernames(username);
+  if (existingUsernames.length == 0) {
+    res.send("ok").end();
+  } else {
+    res.send("Username taken").end();
+  }
+});
+
+accountRoutes.post("/email-validation", async (req, res) => {
+  let email = req.body.email_input;
+  let existingEmails = await user.existingEmails(email);
+  if (existingEmails.length == 0) {
+    res.send("ok").end();
+  } else {
+    res.send("Email taken").end();
+  }
+});
+
+accountRoutes.post("/login", async (req, res) => {
+  let email = req.body.email_input;
+  let password = req.body.password_input;
+  let existingEmails = await user.existingEmails(email);
+  if (existingEmails.length > 0) {
+    if (bcrypt.compareSync(password, existingEmails[0].password)) {
+      req.session.loggedin = true;
+      req.session.username = existingEmails[0].name;
+      req.session.userId = existingEmails[0].id;
+      res.status(200).end();
+    } else {
+      res.status(422).send("Incorect password.");
+    }
+  } else {
+    res.status(422).send("No account with this email.");
+  }
+});
+
 accountRoutes.post("/register", async (req, res) => {
   let username = req.body.username;
   let email = req.body.email_input;
-  if (req.body.password_input != req.body.password_confirm) {
-    res.status(422).send("passwords must match");
-  } else {
+  let password = req.body.password_input;
+  if (username && email && password) {
     let existingUsernames = await user.existingUsernames(username);
     let existingEmails = await user.existingEmails(email);
     if (existingUsernames.length == 0) {
@@ -34,54 +71,13 @@ accountRoutes.post("/register", async (req, res) => {
         req.session.userId = await user.getID(username);
         res.status(200).end();
       } else {
-        res.status(422).send("email already taken");
+        res.status(422).send("Email already taken");
       }
     } else {
-      res.status(422).send("username already taken");
+      res.status(422).send("Username already taken");
     }
   }
 });
-
-accountRoutes.post("/verifying-login", async (req, res) => {
-  let is_email = undefined;
-  let is_password = undefined;
-  let email = req.body.email_input;
-  let password = req.body.password_input;
-  let existing_email = await user.existingEmails(email);
-  if (existing_email.length > 0) {
-    is_email = true;
-  } else {
-    is_email = false;
-  }
-  if (bcrypt.compareSync(password, verifyingUser[0].password)) {
-    is_password = true;
-  } else {
-    is_password = false;
-  }
-  let data = JSON.stringify({
-    is_email: is_email,
-    is_password: is_password,
-  });
-  res.send(data).end();
-});
-
-// accountRoutes.post("/login", async (req, res) => {
-//   let email = req.body.email_input;
-//   let password = req.body.password_input;
-//   let verifiedUser = await user.verifyThroughEmail(email);
-//   if (verifiedUser.length > 0) {
-//     if (bcrypt.compareSync(password, verifiedUser[0].password)) {
-//       req.session.loggedin = true;
-//       req.session.username = verifiedUser[0].name;
-//       req.session.userId = verifiedUser[0].id;
-//       res.status(200).end();
-//     } else {
-//       res.status(422).send("incorect password");
-//     }
-//   } else {
-//     res.status(422).send("no account with this email");
-//   }
-// });
 
 accountRoutes.get("/logout", (req, res) => {
   req.session.destroy();
--- public/register.js
+++ public/register.js
@@ -1,21 +1,82 @@
 $(document).ready(function () {
-  $("#register-form").on("submit", function (e) {
+  let correct_username = false;
+  let correct_email = false;
+  let secured_password = false;
+  let password_confirmation = false;
+
+  $("#email_input").on("input", function () {
+    $(this).removeClass("is-valid is-invalid");
+    let input = $(this)[0];
+    let data = $(this);
+    if (input.checkValidity()) {
+      $.ajax({
+        type: "post",
+        url: "/email-validation",
+        data: data,
+        complete: function (data) {
+          if (data.responseText === "ok") {
+            correct_email = true;
+            $("#email_input").addClass("is-valid");
+          } else {
+            correct_email = false;
+            $("#email_input").addClass("is-invalid");
+            $("#email-invalid").text("Email already taken.");
+          }
+        },
+      });
+    } else {
+      correct_email = false;
+      $(this).addClass("is-invalid");
+      $("#email-invalid").text("Must be a valid email address.");
+    }
+  });
+
+  $("#password_input").on("input", function () {
+    $(this).removeClass("is-valid is-invalid");
+    let input = $(this)[0];
+    if (input.checkValidity()) {
+      secured_password = true;
+      $(this).addClass("is-valid");
+    } else {
+      secured_password = false;
+      $(this).addClass("is-invalid");
+    }
+  });
+
+  $(".psw").on("input", function () {
+    $("#password_confirm").removeClass("is-valid is-invalid");
+    if ($("#password_confirm").val() != "") {
+      if ($("#password_input").val() != $("#password_confirm").val()) {
+        $("#password_confirm").addClass("is-invalid");
+        password_confirm = false;
+      } else {
+        $("#password_confirm").addClass("is-valid");
+        password_confirm = true;
+      }
+    }
+  });
+
+  $("#register-form").submit(function (e) {
     e.preventDefault();
-    let data = $(this).serialize();
+    let form = $(this)[0];
+    let formData = $(this).serialize();
+    let is_valid =
+      correct_username &&
+      correct_email &&
+      secured_password &&
+      password_confirmation;
+    if (!is_valid) {
+      e.stopPropagation();
+    }
     $.ajax({
       type: "post",
       url: "/register",
-      data: data,
-      dataType: "text",
+      data: formData,
       success: function () {
         window.location.href = "http://localhost:3000/";
       },
       error: function (data) {
-        $("#errors").html(
-          "<div class='alert alert-danger' role='alert'>" +
-            data.responseText +
-            "</div>"
-        );
+        console.log(data);
       },
     });
   });
