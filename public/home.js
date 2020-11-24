111e729602f7cf687316f38dd8bdad8be752ba09
Alexis Simonian <alexis792.as@gmail.com>
2020-11-24 23:27:33 +0100

    hover left

--- public/home.js
+++ public/home.js
@@ -5,7 +5,7 @@ $.ajax({
   complete: function (xhr) {
     let welcomeMsg = xhr.getResponseHeader("welcomeMsg");
     let logbtn = xhr.getResponseHeader("logbtn");
-    $("#welcomeMsg").text(welcomeMsg);
+    $("#welcome_msg").text(welcomeMsg);
     $("#logbtn").html(`<a href='/${logbtn}'>${logbtn}</a>`);
   },
 });
@@ -15,13 +15,24 @@ let date_1;
 let date_2;
 
 function setDates() {
-  date_1 = $.datepicker.parseDate("mm/dd/yy", $("#check_in").val());
-  date_2 = $.datepicker.parseDate("mm/dd/yy", $("#check_out").val());
+  date_1 = $.datepicker.parseDate("yy-mm-dd", $("#check_in").val());
+  date_2 = $.datepicker.parseDate("yy-mm-dd", $("#check_out").val());
 }
 
-$(".forward").datepicker({
-  numberOfMonths: 2,
+function refreshDates() {
+  $(".datepicker").datepicker("refresh");
+}
 
+let now = new Date();
+let day = String(now.getDate()).padStart(2, "0");
+let month = String(now.getMonth() + 1).padStart(2, "0");
+let year = now.getFullYear();
+let today = year + "-" + month + "-" + day;
+
+$.datepicker.setDefaults({
+  minDate: today,
+  dateFormat: "yy-mm-dd",
+  numberOfMonths: 2,
   beforeShow: function (input, inst) {
     setTimeout(() => {
       inst.dpDiv.css({
@@ -30,7 +41,6 @@ $(".forward").datepicker({
       });
     }, 0);
   },
-
   beforeShowDay: function (date) {
     setDates();
     if (date_1 && date.getTime() == date_1.getTime()) {
@@ -40,92 +50,60 @@ $(".forward").datepicker({
     }
     return [true, ""];
   },
+});
 
+$(".check_in").datepicker({
   onSelect: function (date, inst) {
-    setDates();
-    if (!date_1) {
-      $("#check_in").val(date);
-    } else if (date_1 && !date_2) {
-      $("#check_out").val(date);
-    } else if (date_1 && date_2) {
-      if (date <= $("#check_in").val()) {
-        $("#check_in").val(date);
-      }
-      if (date >= $("#check_in").val()) {
-        $("#check_out").val(date);
-      }
+    $("#check_in").val(date);
+    refreshDates();
+    if (!date_2) {
+      $(this).hide();
+      $("#check_out").click();
     }
   },
 });
 
-$(".backward").datepicker({
-  numberOfMonths: 2,
-
-  beforeShow: function (input, inst) {
-    setTimeout(() => {
-      inst.dpDiv.css({
-        top: 150,
-        left: 0,
-      });
-    }, 0);
-  },
-
-  beforeShowDay: function (date) {
-    setDates();
-    if (date_1 && date.getTime() == date_1.getTime()) {
-      return [true, "ui-state-highlight"];
-    } else if (date_2 && date >= date_1 && date <= date_2) {
-      return [true, "ui-state-highlight"];
-    }
-    return [true, ""];
-  },
-
+$(".check_out").datepicker({
   onSelect: function (date, inst) {
-    setDates();
+    $("#check_out").val(date);
+    refreshDates();
     if (!date_1) {
-      $("#check_in").val(date);
-    } else if (date_1 && !date_2) {
-      $("#check_out").val(date);
-    } else if (date_1 && date_2) {
-      if (date <= $("#check_in").val()) {
-        $("#check_in").val(date);
-      }
-      if (date >= $("#check_in").val()) {
-        $("#check_out").val(date);
-      }
+      $(this).hide();
+      $("#check_in").click();
     }
   },
 });
 
+//Which datepicker shows up
 $(".datepicker").hide();
 
 $("#check_in").click(function () {
-  if ($(".backward").is(":visible")) {
-    $(".backward").hide();
-    $(".forward").show();
-  } else if ($(".forward").is(":visible")) {
-    $(".forward").hide();
+  if ($(".check_out").is(":visible")) {
+    $(".check_out").hide();
+    $(".check_in").show();
+  } else if ($(".check_in").is(":visible")) {
+    $(".check_in").hide();
   } else {
-    $(".forward").show();
+    $(".check_in").show();
   }
 });
 
 $("#check_out").click(function () {
-  if ($(".forward").is(":visible")) {
-    $(".forward").hide();
-    $(".backward").show();
-  } else if ($(".backward").is(":visible")) {
-    $(".backward").hide();
+  if ($(".check_in").is(":visible")) {
+    $(".check_in").hide();
+    $(".check_out").show();
+  } else if ($(".check_out").is(":visible")) {
+    $(".check_out").hide();
   } else {
-    $(".backward").show();
+    $(".check_out").show();
   }
 });
 
-// Close date picker when click elsewhere
+// Close datepickers when click elsewhere
 $(document).on("click", function (e) {
   var element = $(e.target);
   if (
-    !element.hasClass("search_dates") &&
+    !element.hasClass("dates") &&
     !element.hasClass("ui-datepicker") &&
     !element.hasClass("ui-icon") &&
     !element.hasClass("ui-datepicker-next") &&
@@ -135,5 +113,13 @@ $(document).on("click", function (e) {
     if ($(".hasDatepicker").is(":visible")) {
       $(".datepicker").hide();
     }
+  }
+});
+
+$(".ui-state-default").on("mouseover", function () {
+  let day = $(this).text();
+  let month = $(this).parents("td").attr("data-month");
+  let year = $(this).parents("td").attr("data-year");
+  if (date_1) {
   }
 });
