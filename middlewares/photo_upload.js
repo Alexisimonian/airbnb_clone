f20c48ad5c8014c1daa4a4c909c396ddc019282e
Alexis Simonian <alexis792.as@gmail.com>
2021-01-18 11:58:31 +0100

    restarting

--- middlewares/photo_upload.js
+++ middlewares/photo_upload.js
@@ -1,25 +1,5 @@
-const cloudinary = require("cloudinary").v2;
-const { CloudinaryStorage } = require("multer-storage-cloudinary");
 const multer = require("multer");
 const util = require("util");
-
-cloudinary.config({
-  cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
-  api_key: process.env.CLOUDINARY_API_KEY,
-  api_secret: process.env.CLOUDINARY_API_SECRET,
-});
-
-const storage = new CloudinaryStorage({
-  cloudinary: cloudinary,
-  params: {
-    folder: "airbnb_clone_houses",
-    allowedFormats: ["jpg", "png", "jpeg"],
-    filename: function (req, file, cb) {
-      let filenamevar = `${Date.now()}-${file.originalname}`;
-      cb(null, filenamevar);
-    },
-  },
-});
 
 let uploadfiles = multer({ storage: storage }).single("avatar");
 let uploadFilesMiddleware = util.promisify(uploadfiles);
