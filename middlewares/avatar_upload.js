eb4cf495a08015dd390c89deee9e8dabee4e0d65
Alexis Simonian <alexis792.as@gmail.com>
2021-01-18 19:02:30 +0100

    typo

--- middlewares/avatar_upload.js
+++ middlewares/avatar_upload.js
@@ -3,23 +3,23 @@ const multerS3 = require("multer-s3");
 const AWS = require("aws-sdk");
 
 const s3 = new AWS.S3({
-    accessKeyId = process.env.AWS_ACCESS_KEY_ID,
-    secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY
-})
+  accessKeyId: process.env.AWS_ACCESS_KEY_ID,
+  secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY,
+});
 
 const uploadS3 = multer({
-    storage: multerS3({
-        s3: s3,
-        acl: 'public-read',
-        bucket: process.env.S3_BUCKET_AVATARS,
-        metadata: (rq, file, cb) => {
-            cb(null, {fieldName: file.fieldname})
-        },
-        key: (req, file, cb) => {
-            cb(null, Date.now().toString() + '-' + file.originalname)
-        }
-    })
-})
+  storage: multerS3({
+    s3: s3,
+    acl: "public-read",
+    bucket: process.env.S3_BUCKET_AVATARS,
+    metadata: (rq, file, cb) => {
+      cb(null, { fieldName: file.fieldname });
+    },
+    key: (req, file, cb) => {
+      cb(null, Date.now().toString() + "-" + file.originalname);
+    },
+  }),
+});
 
-let upload = uploadS3
-module.exports = upload
+let upload = uploadS3;
+module.exports = upload;
