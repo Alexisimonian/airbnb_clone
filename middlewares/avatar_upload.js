44e8c591e5cc00026e4cacd954efdda2a26b1d61
Alexis Simonian <alexis792.as@gmail.com>
2021-01-18 11:41:07 +0100

    hi

--- middlewares/avatar_upload.js
+++ middlewares/avatar_upload.js
@@ -9,6 +9,10 @@ cloudinary.config({
   api_secret: process.env.CLOUDINARY_API_SECRET,
 });
 
+console.log(process.env.CLOUDINARY_CLOUD_NAME);
+console.log(process.env.CLOUDINARY_API_KEY);
+console.log(process.env.CLOUDINARY_API_SECRET);
+
 const storage = new CloudinaryStorage({
   cloudinary: cloudinary,
   params: {
