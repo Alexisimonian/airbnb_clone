24da3bd8d27150a4475bb71a415ae3bef835af2c
Alexis Simonian <alexis792.as@gmail.com>
2021-01-18 12:26:16 +0100

    rebuilding app

--- middlewares/avatar_upload.js
+++ middlewares/avatar_upload.js
@@ -1,28 +1,6 @@
-const multer = require("multer");
-const util = require("util");
+// const multer = require("multer");
+// const util = require("util");
 
-cloudinary.config({
-  cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
-  api_key: process.env.CLOUDINARY_API_KEY,
-  api_secret: process.env.CLOUDINARY_API_SECRET,
-});
-
-console.log(process.env.CLOUDINARY_CLOUD_NAME);
-console.log(process.env.CLOUDINARY_API_KEY);
-console.log(process.env.CLOUDINARY_API_SECRET);
-
-const storage = new CloudinaryStorage({
-  cloudinary: cloudinary,
-  params: {
-    folder: "airbnb_clone_avatars",
-    allowedFormats: ["jpg", "png", "jpeg"],
-    filename: function (req, file, cb) {
-      let filenamevar = `${Date.now()}-${file.originalname}`;
-      cb(null, filenamevar);
-    },
-  },
-});
-
-let uploadfiles = multer({ storage: storage }).single("avatar");
-let uploadFilesMiddleware = util.promisify(uploadfiles);
-module.exports = uploadFilesMiddleware;
+// let uploadfiles = multer({ storage: storage }).single("avatar");
+// let uploadFilesMiddleware = util.promisify(uploadfiles);
+// module.exports = uploadFilesMiddleware;
--- middlewares/photo_upload.js
+++ middlewares/photo_upload.js
@@ -1,6 +1,6 @@
-const multer = require("multer");
-const util = require("util");
+// const multer = require("multer");
+// const util = require("util");
 
-let uploadfiles = multer({ storage: storage }).single("avatar");
-let uploadFilesMiddleware = util.promisify(uploadfiles);
-module.exports = uploadFilesMiddleware;
+// let uploadfiles = multer({ storage: storage }).single("avatar");
+// let uploadFilesMiddleware = util.promisify(uploadfiles);
+// module.exports = uploadFilesMiddleware;
